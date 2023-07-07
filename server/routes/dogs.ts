04ad063736360076f4b45ebc1a66cae9ad4802dd
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-08 09:56:32 +1200

    the getselect dog backend work

--- server/routes/dogs.ts
+++ server/routes/dogs.ts
@@ -3,6 +3,7 @@ import * as db from '../db/db'
 
 const router = express.Router()
 
+//Get all dogs
 router.get('/', async (req: Request, res: Response) => {
   try {
     const dogs = await db.getAllDogs()
@@ -14,25 +15,24 @@ router.get('/', async (req: Request, res: Response) => {
   }
 })
 
-router.post('/', async (req: Request, res: Response) => {
-  try {
-    console.log('I am in the server', req.body.id)
-    const id = Number(req.body.id)
-    db.createSelectedDog(id)
-    res.sendStatus(201)
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).json({ error: error.message })
-    }
-  }
-})
+// router.post('/', async (req: Request, res: Response) => {
+//   try {
+//     console.log('I am in the server', req.body.id)
+//     const id = Number(req.body.id)
+//     db.createSelectedDog(id)
+//     res.sendStatus(201)
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       res.status(500).json({ error: error.message })
+//     }
+//   }
+// })
 
-router.get('/detail', async (req: Request, res: Response) => {
+router.get('/detail/:id', async (req: Request, res: Response) => {
   try {
-    const id = Number(req.body.id)
+    const id = Number(req.params.id)
     const selectedDog = await db.getSelectedDog(id)
-
-    res.json(selectedDog)
+    res.json({ selectedDog })
   } catch (error) {
     if (error instanceof Error) {
       res.status(500).json({ error: error.message })
